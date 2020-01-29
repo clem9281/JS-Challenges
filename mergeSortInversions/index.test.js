c3d90d8d8de98aa8606eb035b90b398cdc199303
clem9281 <clem9281@gmail.com>
2020-01-29 13:32:15 -0700

    adds first pass solution for shortest reach, needs tests, and adds tests for libraries and roads

--- mergeSortInversions/index.test.js
+++ mergeSortInversions/index.test.js
@@ -1,7 +1,7 @@
 const { countInversions } = require("./index");
 
 describe("mergeSort countInversions", () => {
-  it("should return 'YES' or 'NO' based on if the strings share a common substring", () => {
+  it("should return the number of inversions", () => {
     expect(countInversions([1, 1, 1, 2, 2])).toBe(0);
     expect(countInversions([2, 1, 3, 1, 2])).toBe(4);
     expect(countInversions([1, 5, 3, 7])).toBe(1);
