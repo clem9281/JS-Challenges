ea5ef6f9606347d158d5b22f8a29d4bb31488cb5
clem9281 <clem9281@gmail.com>
2020-02-18 14:26:05 -0700

    adds solution for productExceptSelf

--- findLongestSubArrayBySum/index.js
+++ findLongestSubArrayBySum/index.js
@@ -10,7 +10,6 @@ function findLongestSubarrayBySum(s, arr) {
     return [-1];
   }
   while (left < arr.length - 1) {
-    // console.log(left, right, range, answer, longestSubset)
     if (range < s && right < arr.length - 1) {
       right++;
       range += arr[right];
@@ -22,14 +21,9 @@ function findLongestSubarrayBySum(s, arr) {
     }
     if (range === s) {
       // we have to look for extra zeroes to consume
-
       while (arr[right + 1] === 0) {
         right++;
       }
-      // while(arr[left - 1] === 0) {
-      //     left--
-      // }
-      // console.log(right, left, right - left > longestSubset)
       if (right - left > longestSubset || longestSubset === 0) {
         longestSubset = right - left;
         answer = [left + 1, right + 1];
@@ -40,7 +34,6 @@ function findLongestSubarrayBySum(s, arr) {
       } else {
         break;
       }
-      // return [left + 1, right + 1]
     }
   }
   return answer;
